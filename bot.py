ca016285e39c709ecf19c07cb0aa2a2b93108bbd
Aditya Enterprises <adityaenterprises@Adityas-MacBook-Air.local>
2025-08-19 22:15:31 +0530

    Clean Railway bot project

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
 import os
 from telegram.ext import Updater, CommandHandler
-
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 def start(update, context):
-    update.message.reply_text("✅ Bot is running on Railway!")
+	update.message.reply_text("✅ Bot is running on Railway!")
 
-if __name__ == "__main__":
-    updater = Updater(TELEGRAM_TOKEN, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    updater.start_polling()
-    updater.idle()
+if __name__== "__main__":
+	if not TELEGRAM_TOKEN:
+		raise ValueError("⚠️ TELEGRAM_TOKEN is not set in environment ariables!")
+
+	updater = Updater(TELEGRAM_TOKEN, use_context=True)
+	dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+	
+	print("🚀Bot started...")
+	updater.start_polling()
+	updater.idle()
+
